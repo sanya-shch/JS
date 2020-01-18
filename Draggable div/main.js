af4ce835ee994a6847b2b657bfc3ba234b3fda88
sanya-shch <shchipkov.sanya@ukr.net>
2020-01-18 15:19:22 +0200

    Draggable div

---
+++ Draggable div/main.js
@@ -0,0 +1,33 @@
+const el = document.querySelector(".item");
+
+let isResizing = false;
+
+el.addEventListener("mousedown", mousedown);
+
+function mousedown(e) {
+    window.addEventListener("mousemove", mousemove);
+    window.addEventListener("mouseup", mouseup);
+
+    let prevX = e.clientX;
+    let prevY = e.clientY;
+
+    function mousemove(e) {
+        if (!isResizing) {
+            let newX = prevX - e.clientX;
+            let newY = prevY - e.clientY;
+
+            const rect = el.getBoundingClientRect();
+
+            el.style.left = rect.left - newX + "px";
+            el.style.top = rect.top - newY + "px";
+
+            prevX = e.clientX;
+            prevY = e.clientY;
+        }
+    }
+
+    function mouseup() {
+        window.removeEventListener("mousemove", mousemove);
+        window.removeEventListener("mouseup", mouseup);
+    }
+}
